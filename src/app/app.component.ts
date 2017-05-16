722f461a5688e1696f5fa059ff6546fe0bf18940
Yamuna Gopalakrishnan <yamuna1612@gmail.com>
2017-05-15 21:47:48 -0500

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+
+import { Component } from '@angular/core';
+
+export class Hero {
+  name: string;
+  id:number;
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>{{hero.name}} details!</h1>
+  <h2>{{hero.id}}</h2>
+  `,
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+    name: 'Windstorm',
+    id: 1
+    };
+}
